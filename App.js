db7df52c75ccf381c61d5bf993f4ea276d683006
Filip Gynthersen <filipg@live.dk>
2024-03-06 20:23:38 +0100

    uGE 4

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, FlatList, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { app, database } from './firebase';
+import { collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [values, loading, error] = useCollection(collection(database, "notes"));
+  const data = values?.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+  async function buttonHandler() {
+    try {
+      await addDoc(collection(database, "notes"), {
+        text: text
+      });
+    } catch (err) {
+      console.log("Fejl i DB " + err);
+    }
+  }
+
+  async function deleteDocument(id) {
+    await deleteDoc(doc(database, "notes", id));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <TextInput style={styles.textInput} onChangeText={(txt) => setText(txt)} />
+        <Button title='Ny Note' onPress={buttonHandler} />
+        <FlatList
+            data={data}
+            renderItem={(note) => (
+                <View style={styles.noteContainer}>
+                  <Text>{note.item.text}</Text>
+                  <TouchableOpacity onPress={() => deleteDocument(note.item.id)}>
+                    <Text style={styles.deleteButton}>Delete</Text>
+                  </TouchableOpacity>
+                </View>
+            )}
+        />
+      </View>
   );
 }
 
@@ -16,5 +48,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 200
   },
+  textInput: {
+    backgroundColor: 'lightblue',
+    minWidth: 200
+  },
+  noteContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5
+  },
+  deleteButton: {
+    marginLeft: 10,
+    color: 'red'
+  }
 });
---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXITxUrRAxm7A7OiJ5J8ZEuNmm-xDvBMg",
+    authDomain: "notes-df73b.firebaseapp.com",
+    projectId: "notes-df73b",
+    storageBucket: "notes-df73b.appspot.com",
+    messagingSenderId: "568101101870",
+    appId: "1:568101101870:web:1c90dd89c0ec97fe9a1abf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app)
+export { app, database }
